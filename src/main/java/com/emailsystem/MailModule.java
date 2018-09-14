025f4fcbe4142b96deea5a9c82351782d9b0fcd0
Itai Bolyasni <itai.bolyasni@dawsoncollege.qc.ca>
2018-09-14 15:47:53 -0400

    Work on processMessage method

--- src/main/java/com/emailsystem/MailModule.java
+++ src/main/java/com/emailsystem/MailModule.java
@@ -45,45 +45,10 @@ public class MailModule {
     private final static Logger LOG = LoggerFactory.getLogger(MailModule.class);
     private final String receiveEmail = "receive.1633867@gmail.com";
     
-    public static void main(String[] args) {
-        EmailBean bean = new EmailBean();
-        bean.setFrom("send.1633867@gmail.com");
-        bean.setTo(new String[] {"receive.1633867@gmail.com"});
-        bean.setSubject("Hello from the other side");
-        bean.setTextMsg("<h1><u>Test:</u> Email thinggy <p> Im sending this using programming and <h3>yes</h3> i am just that cool. </p>");
-        //bean.setCc(new String[] {"cc1@gmail.com", "cc2@gmail.com"});
-//        bean.setHTMLMsg("<html><META http-equiv=Content-Type "
-//                            + "content=\"text/html; charset=utf-8\">"
-//                            + "<body><h1>Here is my photograph embedded in "
-//                            + "this email.</h1>"
-//                            + "<h2>I'm flying!</h2></body></html>");
-        List<AttachmentBean> attach = new ArrayList<AttachmentBean>();
-        List<AttachmentBean> embed = new ArrayList<AttachmentBean>();
-        try {
-            embed.add(new AttachmentBean("thumbsup.jpg", Files.readAllBytes(new File("thumbsup.jpg").toPath())));
-            embed.add(new AttachmentBean("java.jpg", Files.readAllBytes(new File("java.jpg").toPath())));
-            attach.add(new AttachmentBean("java.jpg", Files.readAllBytes(new File("java.jpg").toPath())));
-            bean.setAttach(attach);
-            bean.setEmbedAttach(embed);
-        } catch (Exception e) {
-            
-        }
-        MailModule mail = new MailModule();
-        mail.send(bean);
-        try {
-            Thread.sleep(5000);
-        } catch (InterruptedException e) {
-            LOG.error("Threaded sleep failed", e);
-            System.exit(1);
-        }
-        mail.receive();
-    }
-    
     public void send(EmailBean bean)  {
         if (verifyBeanData(bean)) {
             sendPlainEmail(bean);
-        }
-                
+        }         
     }
     
     public EmailBean[] receive() {
@@ -99,28 +64,14 @@ public class MailModule {
                     beans = new EmailBean[emails.length];
                     for (ReceivedEmail email : emails) {
                         beans[index] = new EmailBean();
-                        System.out.println("\n\n===[" + email.messageNumber() + "]===");
 
-                        // common info
-                        System.out.println("FROM:" + email.from());
+                        // set the bean
                         beans[index].setFrom(email.from().toString());
-                        
-                        // Must have at least one to address
-                        System.out.print("TO:");
-                        System.out.println(Arrays.toString(email.to()));
                         beans[index].setTo(emailArrayToStringArray(email.to()));
-                        if (email.cc().length != 0) {
-                            System.out.print("CC:");
-                            System.out.println(Arrays.toString(email.cc()));
-                            beans[index].setCc(emailArrayToStringArray(email.cc()));
-                        }
-                        System.out.println("SUBJECT:" + email.subject());
+                        beans[index].setCc(emailArrayToStringArray(email.cc()));
                         beans[index].setSubject(email.subject());
-                        System.out.println("PRIORITY:" + email.priority());
                         beans[index].setPriority(email.priority());
-                        System.out.println("SENT DATE:" + email.sentDate());
                         beans[index].setSentTime(LocalDateTime.ofInstant(email.sentDate().toInstant(), ZoneId.systemDefault()));
-                        System.out.println("RECEIVED DATE: " + email.receivedDate());
                         beans[index].setReceivedTime(LocalDateTime.ofInstant(email.receivedDate().toInstant(), ZoneId.systemDefault()));
                         
                         
@@ -130,52 +81,47 @@ public class MailModule {
                         for (EmailMessage msg: messages) {
                             LOG.info("------");
                             if (msg.getMimeType().equalsIgnoreCase("Text/Plain")) {
-                                System.out.println("============================================ PLAIN =============================================================");
                                 beans[index].setTextMsg(msg.getContent());
                             } else if (msg.getMimeType().equalsIgnoreCase("Text/Html")) {
-                                System.out.println("============================================ HTML =============================================================");
                                 beans[index].setHTMLMsg(msg.getContent());
                             }
-                            LOG.info("Encoding: " + msg.getEncoding());
                         }
-
                         // process attachments
-                        List<EmailAttachment<? extends DataSource>> attachments = email.attachments();
-                        if (attachments != null) {
-                            List<AttachmentBean> embed = new ArrayList<>();
-                            List<AttachmentBean> normalAttach = new ArrayList<>();
-                            for (EmailAttachment attach: attachments) {
-                                if (attach.isEmbedded()) {
-                                    embed.add(new AttachmentBean(attach.getName(), attach.toByteArray()));
-                                } else {
-                                    normalAttach.add(new AttachmentBean(attach.getName(), attach.toByteArray()));
-                                }
-                            }
-                            beans[index].setAttach(normalAttach);
-                            beans[index].setEmbedAttach(embed);
+                            beans[index].setAttach(processAttachments(email.attachments(), false));
+                            beans[index].setEmbedAttach(processAttachments(email.attachments(), true));
                         }
                         index++;
                     }
                     return beans;
-                } else {
-                    System.out.println("No emails on IMAP server");
                 } 
-            }
-            
-        } else {
+            } else {
             LOG.info("Unable to send email because either send or recieve addresses are invalid");
         }  
         return null;
     }
     
-    private String[] emailArrayToStringArray(EmailAddress[] emails) {
-        String[] stringEmails = new String[emails.length];
-        for (int i = 0; i < emails.length; i++) {
-            stringEmails[i] = emails[i].toString();
-        }
-        return stringEmails;
+    private String processMessages(EmailMessage msg) {
+        
     }
 
+
+    
+    private List<AttachmentBean> processAttachments(List<EmailAttachment <?extends DataSource>> attachList, boolean embed) {
+        List<AttachmentBean> beanList = new ArrayList<>();
+        for (EmailAttachment attach : attachList) {
+            if (attachList != null) {
+                if (embed && attach.isEmbedded()) {
+                    // get all embed files
+                    beanList.add(new AttachmentBean(attach.getName(), attach.toByteArray()));
+                } else if (!embed && !attach.isEmbedded()) {
+                    // get all attached filess
+                    beanList.add(new AttachmentBean(attach.getName(), attach.toByteArray()));
+                }
+            }
+        }
+        return beanList;
+    }
+    
     
     private void sendPlainEmail(EmailBean bean) {
             SmtpModule smtpServer = new SmtpModule(bean.getFrom(), password);
@@ -206,6 +152,15 @@ public class MailModule {
                 mailSession.close();
             }
     }
+    
+    private String[] emailArrayToStringArray(EmailAddress[] emails) {
+        String[] stringEmails = new String[emails.length];
+        for (int i = 0; i < emails.length; i++) {
+            stringEmails[i] = emails[i].toString();
+        }
+        return stringEmails;
+    }
+
     
     private boolean verifyBeanData(EmailBean bean) {
 
